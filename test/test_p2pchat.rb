061a8a4356d0809436227f5ddc14997a7e10a8ab
Daniil Kharitonov <dan.haritonoff@gmail.com>
2023-02-04 15:48:10 +0100

    test: remove default generated tests

--- test/test_p2pchat.rb
+++ test/test_p2pchat.rb
@@ -3,11 +3,4 @@
 require 'test_helper'
 
 class TestP2PChat < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::P2PChat::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
